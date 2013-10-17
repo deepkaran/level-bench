6727520cc472b77d1088450cc1cd7a427d454377
deepkaran <deepkaran.salooja@gmail.com>
2013-10-17 15:03:46 +0530

    Add readlines function to source

--- random_source.go
+++ random_source.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"math/rand"
 	"strconv"
+	"os"
+	"bufio"
 ) 
 
 type RandomSource struct {
@@ -53,4 +55,21 @@ func (rs *RandomSource) OneValue() (string) {
 	
 	return rs.masterValues[rand.Intn(rs.nValues)]
 
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+  file, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+ 
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
 }
